393b15ba294354f2db90b824f86a8eac6365e235
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-17 21:52:11 +0000

    format code

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import game
+
 print("Welcome to the guessing game! All number must be integers.")
-l=int(input("Please enter a lower bound: "))
-r=int(input("Please enter an upper bound: "))
-game = game.Game(l,r)
+
+l = int(input("Please enter a lower bound: "))
+r = int(input("Please enter an upper bound: "))
+game = game.Game(l, r)
 num = int(input("Please enter a number to guess: "))
+
 while game.guess(num) != 0:
+
     if (game.guess(num) == -1):
         print("You guessed too low!")
+
     else:
         print("You guessed too high!")
+
     num = int(input("Please enter a number to guess: "))
+
 print("You win the game!")
