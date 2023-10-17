8d340a50f06da55d46564ae7f5389640692668a9
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-17 22:08:22 +0000

    Better formatting

--- game.py
+++ game.py
@@ -4,13 +4,14 @@ import random
 
 class Game:
     """Class representing a number gussing game"""
+
     def __init__(self, low: int, high: int):
         self.number = random.randint(low, high)
 
-    def guess(self, n: int):
+    def guess(self, n: int) -> int:
         """Function to check if a guess is correct"""
         return 1 if n > self.number else (-1 if n < self.number else 0)
-    def reset(self, low:int, high:int):
+
+    def reset(self, low: int, high: int) -> None:
         """Function to reset the number to be guessed"""
         self.number = random.randint(low, high)
-    
