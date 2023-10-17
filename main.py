3290b0d90161843a6ee1bbe7c31400da22915abb
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-17 21:41:18 +0000

    fixed another bug

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import game
 game = game.Game()
 
 print("Welcome to the guessing game!")
-num=int(input("Please enter a number: "))
-while game.guess(num)!=1:
-    if(game.guess(num)==-1):
+num = int(input("Please enter a number: "))
+while game.guess(num) != 0:
+    if (game.guess(num) == -1):
         print("You guessed too low!")
     else:
         print("You guessed too high!")
-    num=int(input("Please enter a number: "))
+    num = int(input("Please enter a number: "))
 print("You win the game!")
