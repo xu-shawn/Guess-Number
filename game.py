a4b5cbbd512a2c4fd3567d36f9e92cd3ad0c2e36
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-17 21:58:27 +0000

    Added documentation

--- game.py
+++ game.py
@@ -1,12 +1,16 @@
+"""Module providing a function to generate random numbers"""
 import random
 
 
 class Game:
+    """Class representing a number gussing game"""
     def __init__(self, low: int, high: int):
         self.number = random.randint(low, high)
 
     def guess(self, n: int):
+        """Function to check if a guess is correct"""
         return 1 if n > self.number else (-1 if n < self.number else 0)
-    
     def reset(self, low:int, high:int):
+        """Function to reset the number to be guessed"""
         self.number = random.randint(low, high)
+    
