ceb35124e8a0a3eb4cca47b9714cd8c233b2f3e3
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-17 21:43:35 +0000

    some formatting

--- game.py
+++ game.py
@@ -3,5 +3,8 @@ import random
 class Game:
     def __init__(self):
         self.number = random.randint(0, 1000)
+
     def guess(self, n: int):
         return 1 if n > self.number else (-1 if n < self.number else 0)
+
+    dfe
