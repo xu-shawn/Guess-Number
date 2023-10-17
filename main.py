d6c4a20eb559ad4731340d8a2da74e1cb85374b2
leoche06 <66166876+leoche06@users.noreply.github.com>
2023-10-17 22:10:37 +0000

    update2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Module providing the number guessing game"""
 import game
-
+import time
 print(r"__        __   _                            _          _   _            ")
 print(r"\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___   ")
 print(r" \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \  ")
@@ -15,9 +15,10 @@ print("All number must be integers.")
 
 l = int(input("Please enter a lower bound: "))
 r = int(input("Please enter an upper bound: "))
+t1=time.time()
 game = game.Game(l, r)
 num = int(input("Please enter a number to guess: "))
-
+count=1
 while game.guess(num) != 0:
 
     if game.guess(num) == -1:
@@ -25,7 +26,9 @@ while game.guess(num) != 0:
 
     else:
         print("You guessed too high!")
-
+    count+=1
     num = int(input("Please enter a number to guess: "))
-
+t2=time.time()
 print("You win the game!")
+print("Time taken: ",round(t2-t1,2)," seconds")
+print("Number of guesses: ",count)
