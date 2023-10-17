c5d9f60d35c7a1937d093eff4608f15ed89e6ff3
leoche06 <66166876+leoche06@users.noreply.github.com>
2023-10-17 21:32:19 +0000

    main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+print("Welcome to the guessing game!")
+num=int(input("Please enter a number: "))
+while game.guess(num)!=1:
+    if(game.guess(num)==-1):
+        print("You guessed too low!")
+    else:
+        print("You guessed too high!")
+    num=int(input("Please enter a number: "))
+print("You win the game!")
