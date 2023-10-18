77cbf3691ee31058d2a6a9a2fdd5a689decd994c
leoche06 <66166876+leoche06@users.noreply.github.com>
2023-10-18 03:45:28 +0000

    update

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 """Module to provide a text interface to the number guessing game"""
-from game import Game
+import time
+import game
 
+print(r"__        __   _                            _          _   _            ")
+print(r"\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___   ")
+print(r" \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \  ")
+print(r"  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/  ")
+print(r"   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__|_| |_|\___|_ ")
+print(r"  __ _ _   _  ___  ___ ___(_)_ __   __ _    __ _  __ _ _ __ ___   ___| |")
+print(r" / _` | | | |/ _ \/ __/ __| | '_ \ / _` |  / _` |/ _` | '_ ` _ \ / _ \ |")
+print(r"| (_| | |_| |  __/\__ \__ \ | | | | (_| | | (_| | (_| | | | | | |  __/_|")
+print(r" \__, |\__,_|\___||___/___/_|_| |_|\__, |  \__, |\__,_|_| |_| |_|\___(_)")
+print(r" |___/                             |___/   |___/                        ")
+print("All number must be integers.")
 
-def input_integer(message: str, warning: str = "Please enter a valid integer. Try again.") -> int:
-    """Safely read input and parse into integers"""
-    while True:
-        try:
-            number = int(input(message))
-            break
-        except ValueError:
-            print(warning)
-    return number
+l = int(input("Please enter a lower bound: "))
+r = int(input("Please enter an upper bound: "))
+t1 = time.time()
+game = game.Game(l, r)
+num = int(input("Please enter a number to guess: "))
+count: int = 1
 
+while game.guess(num) != 0:
+    if game.guess(num) == -1:
+        print("You guessed too low!")
+    else:
+        print("You guessed too high!")
+    count += 1
+    if count==5:
+        print("You already guessed 5 times! keep going!")
+    if count==10:
+        print("You already guessed 10 times! you are getting there!")
+    num = int(input("Please enter a number to guess: "))
 
-def main() -> None:
-    """Main function"""
-    Game.welcome()
-
-    l = input_integer("Please enter the lower bound: ")
-    r = input_integer("Please enter the upper bound: ")
-
-    while r < l:
-        r = input_integer("Upper bound must be greater or equal to lower bound! Try again." +
-                          "\nPlease enter the upper bound: ")
-
-    num = input_integer("Please enter a number to guess: ")
-
-    game = Game(l, r)
-    count: int = 1
-
+t2 = time.time()
+print("You win the game!")
+print("Time taken: ", round(t2 - t1, 2), " seconds")
+print("Number of guesses: ", count)
     while game.guess(num) != 0:
         if game.guess(num) == -1:
             print("You guessed too low!")
