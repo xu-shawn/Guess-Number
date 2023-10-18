0ff1dba7f3e6e0fe9ffa457b8594ac1ce568cbe0
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-18 03:37:00 +0000

    Refactor code

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 """Module providing a number guessing game"""
 import random
+import time
 
 
 class Game:
@@ -7,11 +8,31 @@ class Game:
 
     def __init__(self, low: int, high: int):
         self.number = random.randint(low, high)
+        self.start_time = time.time()
 
     def guess(self, n: int) -> int:
         """Function to check if a guess is correct"""
         return 1 if n > self.number else (-1 if n < self.number else 0)
 
+    def get_time(self) -> float:
+        """Function to get the number of seconds that has elapsed since the game begun"""
+        return time.time() - self.start_time
+
     def reset(self, low: int, high: int) -> None:
         """Function to reset the number to be guessed"""
         self.number = random.randint(low, high)
+        self.start_time = time.time()
+
+    @staticmethod
+    def welcome() -> None:
+        """Function to print a welcome message"""
+        print(r"__        __   _                            _          _   _            ")
+        print(r"\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___   ")
+        print(r" \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \  ")
+        print(r"  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/  ")
+        print(r"   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__|_| |_|\___|_ ")
+        print(r"  __ _ _   _  ___  ___ ___(_)_ __   __ _    __ _  __ _ _ __ ___   ___| |")
+        print(r" / _` | | | |/ _ \/ __/ __| | '_ \ / _` |  / _` |/ _` | '_ ` _ \ / _ \ |")
+        print(r"| (_| | |_| |  __/\__ \__ \ | | | | (_| | | (_| | (_| | | | | | |  __/_|")
+        print(r" \__, |\__,_|\___||___/___/_|_| |_|\__, |  \__, |\__,_|_| |_| |_|\___(_)")
+        print(r" |___/                             |___/   |___/                        ")
--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 """Module to provide a text interface to the number guessing game"""
-import time
-import game
+from game import Game
 
-print(r"__        __   _                            _          _   _            ")
-print(r"\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___   ")
-print(r" \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \  ")
-print(r"  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/  ")
-print(r"   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__|_| |_|\___|_ ")
-print(r"  __ _ _   _  ___  ___ ___(_)_ __   __ _    __ _  __ _ _ __ ___   ___| |")
-print(r" / _` | | | |/ _ \/ __/ __| | '_ \ / _` |  / _` |/ _` | '_ ` _ \ / _ \ |")
-print(r"| (_| | |_| |  __/\__ \__ \ | | | | (_| | | (_| | (_| | | | | | |  __/_|")
-print(r" \__, |\__,_|\___||___/___/_|_| |_|\__, |  \__, |\__,_|_| |_| |_|\___(_)")
-print(r" |___/                             |___/   |___/                        ")
-print("All number must be integers.")
+def main() -> None:
+    """Main function"""
+    Game.welcome()
 
-l = int(input("Please enter a lower bound: "))
-r = int(input("Please enter an upper bound: "))
-t1 = time.time()
-game = game.Game(l, r)
-num = int(input("Please enter a number to guess: "))
-count: int = 1
-
-while game.guess(num) != 0:
-    if game.guess(num) == -1:
-        print("You guessed too low!")
-    else:
-        print("You guessed too high!")
-    count += 1
+    l = int(input("Please enter a lower bound: "))
+    r = int(input("Please enter an upper bound: "))
     num = int(input("Please enter a number to guess: "))
 
-t2 = time.time()
-print("You win the game!")
-print("Time taken: ", round(t2 - t1, 2), " seconds")
-print("Number of guesses: ", count)
+    game = Game(l, r)
+    count: int = 1
+
+    while game.guess(num) != 0:
+        if game.guess(num) == -1:
+            print("You guessed too low!")
+        else:
+            print("You guessed too high!")
+        count += 1
+        num = int(input("Please enter a number to guess: "))
+
+    print("You win the game!")
+    print("Time taken: ", round(game.get_time(), 2), " seconds")
+    print("Number of guesses: ", count)
+
+if __name__ == "__main__":
+    main()
