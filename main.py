b7f8e64be9d66a3a6023d207794c1ee9c298fa83
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2023-10-18 03:43:56 +0000

    Added safe input

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 """Module to provide a text interface to the number guessing game"""
 from game import Game
 
+
+def input_integer(message: str, warning: str = "Please enter an integer. Try again.") -> int:
+    """Safely read input and parse into integers"""
+    while True:
+        try:
+            number = int(input(message))
+            break
+        except ValueError:
+            print(warning)
+    return number
+
+
 def main() -> None:
     """Main function"""
     Game.welcome()
 
-    l = int(input("Please enter a lower bound: "))
-    r = int(input("Please enter an upper bound: "))
-    num = int(input("Please enter a number to guess: "))
+    l = input_integer("Please enter a lower bound: ")
+    r = input_integer("Please enter an upper bound: ")
+    num = input_integer("Please enter a number to guess: ")
 
     game = Game(l, r)
     count: int = 1
@@ -18,11 +30,12 @@ def main() -> None:
         else:
             print("You guessed too high!")
         count += 1
-        num = int(input("Please enter a number to guess: "))
+        num = input_integer("Please enter a number to guess: ")
 
     print("You win the game!")
     print("Time taken: ", round(game.get_time(), 2), " seconds")
     print("Number of guesses: ", count)
+
 
 if __name__ == "__main__":
     main()
